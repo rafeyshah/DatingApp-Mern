ff8b2ffb137791a0826b335890cb6cfa70d8661e
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2022-02-01 16:24:19 +0500

    main__swipeButtons__frontEnd

--- tinder-react/src/App.js
+++ tinder-react/src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
 import Header from './Header';
 import TinderCards from './TinderCards';
+import SwipeButtons from './SwipeButtons';
 
 function App() {
     return (
         // BEM class naming convention
         <div className="app" >
 
-            {/* Header */}
             <Header />
-            {/* TinderCards */}
             <TinderCards />
-            {/* SwipeButtons */}
+            <SwipeButtons />
         </div>
     );
 }
