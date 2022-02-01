67e5c7df015e6eeee54dc9aba0a3fc85d5632211
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2022-02-01 14:25:53 +0500

    main__header__frontend

---
+++ tinder-react/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+    return (
+        // BEM class naming convention
+        <div className="app" >
+
+            {/* Header */}
+            <Header />
+            {/* TinderCards */}
+            {/* SwipeButtons */}
+        </div>
+    );
+}
+
+export default App;
